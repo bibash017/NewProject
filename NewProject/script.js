a5abe0d6d35e11d7820fcec3f23d8eaef3c90275
bibash017 <150315963+bibash017@users.noreply.github.com>
2025-04-27 23:23:00 -0500

    Add files via upload

---
+++ NewProject/script.js
@@ -0,0 +1,6 @@
+function changeText() {
+    const message = document.getElementById('message');
+    message.textContent = "Thanks for clicking! 🌟";
+    message.style.color = "#6a11cb";
+    message.style.fontWeight = "bold";
+}
